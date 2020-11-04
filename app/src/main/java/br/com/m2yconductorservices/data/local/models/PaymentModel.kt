cf13720db71b7d559844db5533dd8cad8e22c5b1
Jose Mateus <mateussantos@dgtalhero.es>
2020-11-04 17:45:45 -0300

    Implemented new payments flow to v1

--- app/src/main/java/br/com/m2yconductorservices/data/local/models/PaymentModel.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/local/models/PaymentModel.kt
@@ -12,13 +12,12 @@ import java.io.Serializable
 import java.lang.Exception
 import java.util.*
 
-class PaymentModel(var messageId: Int = 0,
+class PaymentModel(
                    var value: Float? = 0f,
                    var valueWithCharges: Float? = 0f,
                    var barcode: String? = null,
                    var description: String? = null,
                    var name: String? = null,
-                   var social: String? = null,
                    var document: String? = null,
                    var discount: Float? = 0f,
                    var interest: Float? = 0f,
@@ -41,20 +40,24 @@ class PaymentModel(var messageId: Int = 0,
                    var openingPaymentSchedule: String? = null,
                    var closingPaymentSchedule: String? = null,
                    var isValidWindow: Boolean? = null,
-                   var message: String? = null,
                    var password: String?,
-                   var cardId: String?) : Serializable {
+                   var cardId: String?
+) : Serializable {
 
     val paymentLimitDisplay: String
-        get() = this.paymentLimitDate?.m2yCdtChangeDateFormat(M2YCDTConstants.TICKET_DATE_FORMAT, M2YCDTConstants.COMMON_DATE_FORMAT)
-                ?: ""
+        get() = this.paymentLimitDate?.m2yCdtChangeDateFormat(
+            M2YCDTConstants.TICKET_DATE_FORMAT,
+            M2YCDTConstants.COMMON_DATE_FORMAT
+        )
+            ?: ""
 
     val paymentLimitIsAfterToday: Boolean
         get() {
             val calToday = Calendar.getInstance()
             val calLimit = Calendar.getInstance()
 
-            val dateLimit = this.paymentLimitDate?.m2yCdtDateFromString(M2YCDTConstants.TICKET_DATE_FORMAT)
+            val dateLimit =
+                this.paymentLimitDate?.m2yCdtDateFromString(M2YCDTConstants.TICKET_DATE_FORMAT)
             return if (dateLimit == null) {
                 false
             } else {
@@ -83,17 +86,17 @@ class PaymentModel(var messageId: Int = 0,
     }
 
     class PaymentTicketJson(
-            var barcode: String?,
-            var bank: String?,
-            var name: String?,
-            var cpfOrCNPJ: String?,
-            var discount: Float?,
-            var interest: Float?,
-            var fine: Float?,
-            var charges: Float?,
-            var expiration: String?, //dd/MM/yyyy
-            var amount: Float?,
-            var paymentDate: String? //dd/MM/yyyy HH:mm:ss
+        var barcode: String?,
+        var bank: String?,
+        var name: String?,
+        var cpfOrCNPJ: String?,
+        var discount: Float?,
+        var interest: Float?,
+        var fine: Float?,
+        var charges: Float?,
+        var expiration: String?, //dd/MM/yyyy
+        var amount: Float?,
+        var paymentDate: String? //dd/MM/yyyy HH:mm:ss
     ) : Serializable {
 
         val totalCharges: Float
@@ -104,121 +107,146 @@ class PaymentModel(var messageId: Int = 0,
 }
 
 fun PaymentModel.toTicketModel(accountId: String): TicketModel {
-    return TicketModel(type = TicketType.PAYMENT, barcode = barcode,
-            amount = value ?: 0f, sourceAccount = accountId, destination = jsonObject?.name ?: "",
-            document = jsonObject?.cpfOrCNPJ ?: document, discount = jsonObject?.discount
+    return TicketModel(
+        type = TicketType.PAYMENT, barcode = barcode,
+        amount = value ?: 0f, sourceAccount = accountId, destination = jsonObject?.name ?: "",
+        document = jsonObject?.cpfOrCNPJ ?: document, discount = jsonObject?.discount
             ?: discount ?: 0f, interest = jsonObject?.interest ?: interest ?: 0f,
-            fine = jsonObject?.fine ?: fine ?: 0f, charges = jsonObject?.charges ?: charges
-    ?: 0f, dueDate = jsonObject?.expiration ?: dueDate)
+        fine = jsonObject?.fine ?: fine ?: 0f, charges = jsonObject?.charges ?: charges
+        ?: 0f, dueDate = jsonObject?.expiration ?: dueDate
+    )
 }
 
 fun PaymentModel.toReceiptModel(): ReceiptModel {
-    val descriptionObject =  Gson().fromJson(description, PaymentTicketResponse.PaymentTicketJson::class.java)
-    return ReceiptModel("", "", jsonObject?.paymentDate?.m2yCdtChangeDateFormat(M2YCDTConstants.CDT_DATE_FORMAT, M2YCDTConstants.COMMON_DATE_TIME_FORMAT), type = ReceiptType.PAYMENT, payment = ReceiptPaymentModel(barcode, jsonObject?.name ?: descriptionObject.name,
-            jsonObject?.expiration, jsonObject?.paymentDate?.m2yCdtChangeDateFormat(M2YCDTConstants.CDT_DATE_FORMAT, M2YCDTConstants.COMMON_DATE_FORMAT), jsonObject?.discount, jsonObject?.fine,
-            jsonObject?.charges, jsonObject?.interest, jsonObject?.cpfOrCNPJ, jsonObject?.paymentDate,
-            jsonObject?.amount, M2YCDTPersistUserInformation.accountId()))
+    val descriptionObject =
+        Gson().fromJson(description, PaymentTicketResponse.PaymentTicketJson::class.java)
+    return ReceiptModel(
+        "",
+        "",
+        jsonObject?.paymentDate?.m2yCdtChangeDateFormat(
+            M2YCDTConstants.CDT_DATE_FORMAT,
+            M2YCDTConstants.COMMON_DATE_TIME_FORMAT
+        ),
+        type = ReceiptType.PAYMENT,
+        payment = ReceiptPaymentModel(
+            barcode,
+            jsonObject?.name ?: descriptionObject.name,
+            jsonObject?.expiration,
+            jsonObject?.paymentDate?.m2yCdtChangeDateFormat(
+                M2YCDTConstants.CDT_DATE_FORMAT,
+                M2YCDTConstants.COMMON_DATE_FORMAT
+            ),
+            jsonObject?.discount,
+            jsonObject?.fine,
+            jsonObject?.charges,
+            jsonObject?.interest,
+            jsonObject?.cpfOrCNPJ,
+            jsonObject?.paymentDate,
+            jsonObject?.amount,
+            M2YCDTPersistUserInformation.accountId()
+        )
+    )
 }
 
 
 fun BarcodeValidationResponse.toPaymentModel(): PaymentModel {
 
     when {
-        DataReturn?.Result?.BillPaymentTypeId == BillPaymentType.NORMAL.serverId ->
-            return PaymentModel(Message?.MessageId ?: -1,
-                    DataReturn.Result?.ValidateBarCode?.Value,
-                    DataReturn.Result?.ValidateBarCode?.Value,
-                    DataReturn.Result?.ValidateBarCode?.BarCodeNumber,
-                    DataReturn.Result?.ValidateBarCode?.Description,
-                    null,
-                    null,
-                    null,
-                    DataReturn.Result?.PaymentInfoNPC?.ComputedBillValues?.DiscountValueCalculated,
-                    DataReturn.Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedInterestAmount,
-                    DataReturn.Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedFineValue,
-                    getCharges(),
-                    if (DataReturn?.Result?.ValidateBarCode?.HasDueDate == false)
-                        java.util.Calendar.getInstance().m2yCdtFormat(M2YCDTConstants.TICKET_DATE_FORMAT)
-                    else
-                        DataReturn.Result?.ValidateBarCode?.DueDate,
-                    null,
-                    null,
-                    null,
-                    DataReturn.Result?.ValidateBarCode?.MinTime,
-                    DataReturn.Result?.ValidateBarCode?.MaxTime,
-                    DataReturn.Result?.ValidateBarCode?.IsOutTime,
-                    null,
-                    DataReturn.Result?.ValidateBarCode?.HasDueDate,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    DataReturn.Result?.Message?.Message, null, null)
-        DataReturn?.Result?.BillPaymentTypeId == BillPaymentType.NPC.serverId ->
-            return PaymentModel(Message?.MessageId ?: -1,
-                DataReturn.Result?.PaymentInfoNPC?.ComputedBillValues?.TotalAmountToCharge,
-                    DataReturn.Result?.PaymentInfoNPC?.ComputedBillValues?.TotalAmountToCharge,
-                    DataReturn.Result?.PaymentInfoNPC?.DigitavelLine,
-                    DataReturn.Result?.ValidateBarCode?.Description,
-                    DataReturn.Result?.PaymentInfoNPC?.Traders?.Recipient,
-                    DataReturn.Result?.PaymentInfoNPC?.Traders?.Recipient,
-                    DataReturn.Result?.PaymentInfoNPC?.Traders?.RecipientDocument,
-                    DataReturn.Result?.PaymentInfoNPC?.ComputedBillValues?.DiscountValueCalculated,
-                    DataReturn.Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedInterestAmount,
-                    DataReturn.Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedFineValue,
-                    getCharges(),
-                    DataReturn.Result?.PaymentInfoNPC?.DueDate,
-                    null,
-                    null,
-                    null,
-                    DataReturn.Result?.ValidateBarCode?.MinTime,
-                    DataReturn.Result?.ValidateBarCode?.MaxTime,
-                    DataReturn.Result?.ValidateBarCode?.IsOutTime,
-                    DataReturn.Result?.PaymentInfoNPC?.PaymentLimitDate,
-                    DataReturn.Result?.ValidateBarCode?.HasDueDate,
-                    DataReturn.Result?.PaymentInfoNPC?.ReceiptRules?.MaxPaymentValue,
-                    DataReturn.Result?.PaymentInfoNPC?.ReceiptRules?.MaxPaymentValue,
-                    DataReturn.Result?.PaymentInfoNPC?.Params?.AptoForPayment,
-                    DataReturn.Result?.PaymentInfoNPC?.Params?.BlockPaymentOfDate,
-                    true,
-                    DataReturn.Result?.PaymentInfoNPC?.OpeningPaymentschedule,
-                    DataReturn.Result?.PaymentInfoNPC?.ClosingPaymentschedule,
-                    DataReturn.Result?.PaymentInfoNPC?.IsValidWindow,
-                    DataReturn.Result?.Message?.Message, null, null)
-        else -> return PaymentModel(Message?.MessageId ?: -1,
-                DataReturn?.Result?.PaymentInfoNPC?.BillValue,
-                DataReturn?.Result?.PaymentInfoNPC?.ComputedBillValues?.TotalAmountToCharge,
-                DataReturn?.Result?.PaymentInfoNPC?.DigitavelLine,
-                DataReturn?.Result?.ValidateBarCode?.Description,
-                DataReturn?.Result?.PaymentInfoNPC?.Traders?.Recipient,
-                DataReturn?.Result?.PaymentInfoNPC?.Traders?.Recipient,
-                DataReturn?.Result?.PaymentInfoNPC?.Traders?.RecipientDocument,
-                DataReturn?.Result?.PaymentInfoNPC?.ComputedBillValues?.DiscountValueCalculated,
-                DataReturn?.Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedInterestAmount,
-                DataReturn?.Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedFineValue,
+        Result?.BillPaymentTypeId == BillPaymentType.NORMAL.serverId ->
+            return PaymentModel(
+                Result?.ValidateBarCode?.Value,
+                Result?.ValidateBarCode?.Value,
+                Result?.ValidateBarCode?.BarCodeNumber,
+                Result?.ValidateBarCode?.Description,
+                null,
+                null,
+                Result?.PaymentInfoNPC?.ComputedBillValues?.DiscountValueCalculated,
+                Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedInterestAmount,
+                Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedFineValue,
                 getCharges(),
-                if (DataReturn?.Result?.ValidateBarCode?.HasDueDate == false) java.util.Calendar.getInstance().m2yCdtFormat(M2YCDTConstants.TICKET_DATE_FORMAT) else DataReturn?.Result?.PaymentInfoNPC?.DueDate,
+                if (Result?.ValidateBarCode?.HasDueDate == false)
+                    java.util.Calendar.getInstance()
+                        .m2yCdtFormat(M2YCDTConstants.TICKET_DATE_FORMAT)
+                else
+                    Result?.ValidateBarCode?.DueDate,
                 null,
                 null,
                 null,
-                DataReturn?.Result?.ValidateBarCode?.MinTime,
-                DataReturn?.Result?.ValidateBarCode?.MaxTime,
-                DataReturn?.Result?.ValidateBarCode?.IsOutTime,
-                DataReturn?.Result?.PaymentInfoNPC?.PaymentLimitDate,
-                DataReturn?.Result?.ValidateBarCode?.HasDueDate,
-                DataReturn?.Result?.PaymentInfoNPC?.ReceiptRules?.MaxPaymentValue,
-                DataReturn?.Result?.PaymentInfoNPC?.ReceiptRules?.MaxPaymentValue,
-                DataReturn?.Result?.PaymentInfoNPC?.Params?.AptoForPayment,
-                DataReturn?.Result?.PaymentInfoNPC?.Params?.BlockPaymentOfDate,
+                Result?.ValidateBarCode?.MinTime,
+                Result?.ValidateBarCode?.MaxTime,
+                Result?.ValidateBarCode?.IsOutTime,
+                null,
+                Result?.ValidateBarCode?.HasDueDate,
                 null,
                 null,
                 null,
                 null,
-                DataReturn?.Result?.Message?.Message, null, null)
+                null,
+                null,
+                null,
+                null,
+                null, null
+            )
+        Result?.BillPaymentTypeId == BillPaymentType.NPC.serverId ->
+            return PaymentModel(
+                Result?.PaymentInfoNPC?.ComputedBillValues?.TotalAmountToCharge,
+                Result?.PaymentInfoNPC?.ComputedBillValues?.TotalAmountToCharge,
+                Result?.ValidateBarCode?.Description,
+                Result?.PaymentInfoNPC?.Traders?.Recipient,
+                Result?.PaymentInfoNPC?.Traders?.Recipient,
+                Result?.PaymentInfoNPC?.Traders?.RecipientDocument,
+                Result?.PaymentInfoNPC?.ComputedBillValues?.DiscountValueCalculated,
+                Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedInterestAmount,
+                Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedFineValue,
+                getCharges(),
+                Result?.PaymentInfoNPC?.DueDate,
+                null,
+                null,
+                null,
+                Result?.ValidateBarCode?.MinTime,
+                Result?.ValidateBarCode?.MaxTime,
+                Result?.ValidateBarCode?.IsOutTime,
+                Result?.PaymentInfoNPC?.PaymentLimitDate,
+                Result?.ValidateBarCode?.HasDueDate,
+                Result?.PaymentInfoNPC?.ReceiptRules?.MaxPaymentValue,
+                Result?.PaymentInfoNPC?.ReceiptRules?.MaxPaymentValue,
+                Result?.PaymentInfoNPC?.Params?.AptoForPayment,
+                Result?.PaymentInfoNPC?.Params?.BlockPaymentOfDate,
+                true,
+                Result?.PaymentInfoNPC?.OpeningPaymentschedule,
+                Result?.PaymentInfoNPC?.ClosingPaymentschedule,
+                Result?.PaymentInfoNPC?.IsValidWindow, null, null
+            )
+        else -> return PaymentModel(
+            Result?.PaymentInfoNPC?.BillValue,
+            Result?.PaymentInfoNPC?.ComputedBillValues?.TotalAmountToCharge,
+            Result?.ValidateBarCode?.Description,
+            Result?.PaymentInfoNPC?.Traders?.Recipient,
+            Result?.PaymentInfoNPC?.Traders?.Recipient,
+            Result?.PaymentInfoNPC?.Traders?.RecipientDocument,
+            Result?.PaymentInfoNPC?.ComputedBillValues?.DiscountValueCalculated,
+            Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedInterestAmount,
+            Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedFineValue,
+            getCharges(),
+            if (Result?.ValidateBarCode?.HasDueDate == false) java.util.Calendar.getInstance()
+                .m2yCdtFormat(M2YCDTConstants.TICKET_DATE_FORMAT) else Result?.PaymentInfoNPC?.DueDate,
+            null,
+            null,
+            null,
+            Result?.ValidateBarCode?.MinTime,
+            Result?.ValidateBarCode?.MaxTime,
+            Result?.ValidateBarCode?.IsOutTime,
+            Result?.PaymentInfoNPC?.PaymentLimitDate,
+            Result?.ValidateBarCode?.HasDueDate,
+            Result?.PaymentInfoNPC?.ReceiptRules?.MaxPaymentValue,
+            Result?.PaymentInfoNPC?.ReceiptRules?.MaxPaymentValue,
+            Result?.PaymentInfoNPC?.Params?.AptoForPayment,
+            Result?.PaymentInfoNPC?.Params?.BlockPaymentOfDate,
+            null,
+            null,
+            null,
+            null, null, null
+        )
     }
 
 
--- app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/BarcodeValidationResponse.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/BarcodeValidationResponse.kt
@@ -1,99 +1,100 @@
 package br.com.m2yconductorservices.data.remote.models.response
 
-data class BarcodeValidationResponse(var Message: MessageResponse?,
-                                     val DataReturn: DataReturnResponse?) {
-    fun getCharges() = (DataReturn?.Result?.PaymentInfoNPC?.ComputedBillValues?.DiscountValueCalculated ?: 0f) + (DataReturn?.Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedInterestAmount
-            ?: 0f) + (DataReturn?.Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedFineValue ?: 0f)
+data class BarcodeValidationResponse(
+    var Barcode: String?,
+    var Result: DataReturnResultResponse?
+) {
+    fun getCharges() = (Result?.PaymentInfoNPC?.ComputedBillValues?.DiscountValueCalculated
+        ?: 0f) + (Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedInterestAmount
+        ?: 0f) + (Result?.PaymentInfoNPC?.ComputedBillValues?.CalculatedFineValue ?: 0f)
 }
 
-data class MessageResponse(var MessageId: Int?,
-                           var Title: String?,
-                           var Message: String?)
-
-data class DataReturnResponse(var Barcode: String?,
-                              var Result: DataReturnResultResponse?)
-
 data class DataReturnResultResponse(
-        val Success: Boolean?,
-        val Message: DataReturnResultMessageResponse?,
-        val BillPaymentTypeId: Int?,
-        val BillPaymentTypeDescription: String?,
-        val HasEnoughBalance: Boolean?,
-        val WasPaid: Boolean?,
-        val PaymentSchedulingDate: String?,
-        val ReachedLimit: Boolean?,
-        val ValidateBarCode: ValidateBarCodeResponse?,
-        val PaymentInfoNPC: PaymentInfoNPCResponse?)
-
-data class DataReturnResultMessageResponse(
-        val MessageId: Int?,
-        val Title: String?,
-        val Message: String?
+    val Success: Boolean?,
+    val BillPaymentTypeId: Int?,
+    val BillPaymentTypeDescription: String?,
+    val HasEnoughBalance: Boolean?,
+    val WasPaid: Boolean?,
+    val PaymentSchedulingDate: String?,
+    val ReachedLimit: Boolean?,
+    val ValidateBarCode: ValidateBarCodeResponse?,
+    val PaymentInfoNPC: PaymentInfoNPCResponse?
 )
 
 data class PaymentInfoNPCResponse(
-        val Id: Int?,
-        val ContractNumber: String?,
-        val IdentificationNumber: String?,
-        val DueDate: String?,
-        val BillValue: Float?,
-        val TotalAmountToCharge: Float?,
-        val TaxBreakValue: Int?,
-        val PaymentLimitDate: String?,
-        val OpeningPaymentschedule: String?,
-        val ClosingPaymentschedule: String?,
-        val IsValidDate: Boolean?,
-        val IsBeforeWindow: Boolean?,
-        val IsValidWindow: Boolean?,
-        val NextUtilDay: String?,
-        val BarCodeNumber: String?,
-        val DigitavelLine: String?,
-        val Traders: TradersResponse?,
-        val ReceivingDivergentValue: ReceivingDivergentValueResponse?,
-        val ReceiptRules: ReceiptRulesResponse?,
-        val ComputedBillValues: ComputedBillValuesResponse?,
-        val BillStatus: BillStatusResponse?,
-        val Params: ParamsResponse?)
-
-data class ReceivingDivergentValueResponse(
-        val Code: Int?,
-        val Description: String?
+    val Id: Int?,
+    val ContractNumber: String?,
+    val IdentificationNumber: String?,
+    val DueDate: String?,
+    val BillValue: Float?,
+    val TaxBreakValue: Int?,
+    val PaymentLimitDate: String?,
+    val OpeningPaymentschedule: String?,
+    val ClosingPaymentschedule: String?,
+    val IsValidDate: Boolean?,
+    val IsBeforeWindow: Boolean?,
+    val IsValidWindow: Boolean?,
+    val NextUtilDay: String?,
+    val BarCodeNumber: String?,
+    val Traders: TradersResponse?,
+    val ReceivingDivergentValue: ReceivingDivergentValueResponse?,
+    val ReceiptRules: ReceiptRulesResponse?,
+    val ComputedBillValues: ComputedBillValuesResponse?,
+    val BillStatus: BillStatusResponse?,
+    val Params: ParamsResponse?
 )
 
-data class TradersResponse(val Recipient: String?,
-                           val RecipientDocument: String?,
-                           val PayerName: String?,
-                           val PayerDocument: String?)
+data class ReceivingDivergentValueResponse(
+    val Code: Int?,
+    val Description: String?
+)
 
-data class ReceiptRulesResponse(val TypeOfPaymentMin: String?,
-                                val TypeOfPaymentMax: String?,
-                                val MinPaymentValue: Float?,
-                                val MaxPaymentValue: Float?)
+data class TradersResponse(
+    val Recipient: String?,
+    val RecipientDocument: String?,
+    val PayerName: String?,
+    val PayerDocument: String?
+)
 
-data class ComputedBillValuesResponse(val CalculatedInterestAmount: Float?,
-                                      val CalculatedFineValue: Float?,
-                                      val DiscountValueCalculated: Float?,
-                                      val TotalAmountToCharge: Float?)
+data class ReceiptRulesResponse(
+    val TypeOfPaymentMin: String?,
+    val TypeOfPaymentMax: String?,
+    val MinPaymentValue: Float?,
+    val MaxPaymentValue: Float?
+)
 
-data class BillStatusResponse(val Code: Int?,
-                              val Description: String?)
+data class ComputedBillValuesResponse(
+    val CalculatedInterestAmount: Float?,
+    val CalculatedFineValue: Float?,
+    val DiscountValueCalculated: Float?,
+    val TotalAmountToCharge: Float?,
+    val PaymentAmountUpdated: Float?,
+    val ComputedDate: String?
+)
 
-data class ParamsResponse(val OutOfDate: Boolean?,
-                          val AptoForPayment: Boolean?,
-                          val BlockPaymentOfDate: Boolean?)
+data class BillStatusResponse(
+    val Code: Int?,
+    val Description: String?
+)
+
+data class ParamsResponse(
+    val OutOfDate: Boolean?,
+    val AptoForPayment: Boolean?,
+    val BlockPaymentOfDate: Boolean?
+)
 
 data class ValidateBarCodeResponse(
-        val Id: Int?,
-        val Description: String?,
-        val Value: Float?,
-        val DueDate: String?, //yyyy-mm-dd
-        val IsOutTime: Boolean?,
-        val TimeWindow: Int?,
-        val MinTime: String?,
-        val MaxTime: String?,
-        val IsDefaultTime: Boolean?,
-        val PaymentType: Int?,
-        val HasDueDate: Boolean?,
-        val BarCodeNumber: String?,
-        val CurrentDate: String? //yyyy-mm-dd
+    val Id: Int?,
+    val Description: String?,
+    val Value: Float?,
+    val DueDate: String?, //yyyy-mm-dd
+    val IsOutTime: Boolean?,
+    val TimeWindow: Int?,
+    val MinTime: String?,
+    val MaxTime: String?,
+    val IsDefaultTime: Boolean?,
+    val PaymentType: Int?,
+    val HasDueDate: Boolean?,
+    val BarCodeNumber: String?,
+    val CurrentDate: String? //yyyy-mm-dd
 )
